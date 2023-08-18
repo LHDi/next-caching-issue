79837f9f59372ae876dcc10e5ac962012664b399
Mohammed Elhadi Baci <elhadi98@gmail.com>
2023-08-18 09:31:31 +0100

    only remove force cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import { cookies } from 'next/headers';
+
 export const runtime = 'edge';
-//
 
 export default async function Home() {
   const data = await fetch('https://hub.dummyapis.com/delay?seconds=5', {
-    next: {
-      revalidate: 1,
-    },
+    //  cache: 'force-cache',
+    next: { revalidate: 1 },
   });
+  const foo = cookies().get('foo');
   return (
     <code>
       <pre>{JSON.stringify(await data.text(), null, 2)}</pre>
